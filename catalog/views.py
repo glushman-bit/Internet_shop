015a0167be3d09653be1dd4e0137eec9526156dc
Ivan Glushenkov <glushman966@gmail.com>
2026-03-28 15:14:31 +0300

    add linters and fixed project

--- catalog/views.py
+++ catalog/views.py
@@ -3,16 +3,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'home.html')
+    return render(request, "home.html")
+
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'Имя : {name},\n'
-              f'Телефон : {phone},\n'
-              f'Сообщение : {message}')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        print(f"Имя : {name},\n" f"Телефон : {phone},\n" f"Сообщение : {message}")
 
-        return HttpResponse(f'Привет {name}, ваши данные приняты.')
-    return render(request,'contacts.html')
+        return HttpResponse(f"Привет {name}, ваши данные приняты.")
+    return render(request, "contacts.html")
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-
-
+from django.urls import include
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog')),
+    path("admin/", admin.site.urls),
+    path("", include("catalog.urls", namespace="catalog")),
 ]
