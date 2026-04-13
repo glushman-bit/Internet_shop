a1b6929d6337e6633ccdc31b3f87d3a72aee00a8
Ivan Glushenkov <glushman966@gmail.com>
2026-04-13 23:21:44 +0300

    update views.py for page

--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,27 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+import datetime
 
-from catalog.models import Contact
+from django.core.paginator import Paginator
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from catalog.models import Contact, Category
 from catalog.models import Product
 
 
-def home(request):
-    # получение 5 последних продуктов
-    products = Product.objects.all()
-    context = {"products": products}
+def product_list(request):
+    products = Product.objects.all().order_by('-id')
 
-    return render(request, "home.html", context)
+    paginator = Paginator(products, 6)
+    page_number = request.GET.get("page")
+    page_object = paginator.get_page(page_number)
+
+    context = {"page_object": page_object}
+
+    return render(request, "product_list.html", context)
 
 
 def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
+    product = Product.objects.get(pk=pk)
     context = {"product": product}
     return render(request, "product_detail.html", context)
 
@@ -34,3 +41,26 @@ def contacts(request):
         print(contact.name)
 
     return render(request, "contacts.html", {"contacts": contact_list})
+
+
+def create_product(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        image = request.FILES.get("image")
+        category = request.POST.get("category")
+        price = request.POST.get("price")
+
+        category, _ = Category.objects.get_or_create(name=category)
+
+        product = Product.objects.create(
+            name=name,
+            description=description,
+            image=image,
+            category=category,
+            price=price,
+        )
+
+        return redirect("catalog:product_detail", pk=product.pk)
+
+    return render(request, "create_product.html")
