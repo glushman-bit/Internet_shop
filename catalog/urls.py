9a0cf3ad11c02f83db31173c0abc3bd2b233a18e
Ivan Glushenkov <glushman966@gmail.com>
2026-04-29 21:53:42 +0300

    create class ProductDetailView, ProductCreateView,ProductUpdateView, ProductDeleteView update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, product_detail, create_product, ProductListView
+from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
+
 # from catalog.views import product_list
 
 app_name = CatalogConfig.name
@@ -9,8 +11,12 @@ app_name = CatalogConfig.name
 urlpatterns = [
     # path("", product_list, name="product_list"),
     path("contacts/", contacts, name="contacts"),
-    path("product_detail/<int:pk>/", product_detail, name="product_detail"),
-    path("create_product/", create_product, name="create_product"),
+    # path("product_detail/<int:pk>/", product_detail, name="product_detail"),
+    # path("create_product/", create_product, name="create_product"),
 
     path("", ProductListView.as_view(), name="product_list"),
+    path("catalog/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("catalog/create/", ProductCreateView.as_view(), name="product_create"),
+    path("catalog/<int:pk>/update/", ProductUpdateView.as_view(), name="product_create"),
+    path("catalog/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
 ]
