54c7bcb5f6decefd9d148300d5a1634bab9755fc
Ivan Glushenkov <glushman966@gmail.com>
2026-05-01 18:16:13 +0300

    create admin for Blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from blog.models import Blog
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "content", "image", "created_at", "is_active",)
+    list_filter = ("title", "created_at", "is_active",)
+    search_fields = ("title", "created_at", "is_active",)
--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from blog.models import Blog
 from catalog.models import Category
 from catalog.models import Contact
 from catalog.models import Product
@@ -23,10 +22,3 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "phone", "email",)
     list_filter = ("name", "phone",)
     search_fields = ("name", "phone",)
-
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ("title", "content", "image", "created_at", "is_active",)
-    list_filter = ("title", "created_at", "is_active",)
-    search_fields = ("title", "created_at", "is_active",)
