bcdc6bf7e174bc0985b695de7f98cf383e0aa883
Ivan Glushenkov <glushman966@gmail.com>
2026-05-02 18:24:01 +0300

    create CRUD for views.py/blog, create urls.py/blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from blog.models import Article
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "blog/article_list.html"
+    context_object_name = "page_object"
+    paginate_by = 10
+    ordering = ["-created_at"]
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "blog/article_detail.html"
+    context_object_name = "article"
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ("title", "content", "image")
+    template_name = "blog/article_form.html"
+    success_url = reverse_lazy("blog:article_list")
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ("title", "content", "image")
+    template_name = "blog/article_form.html"
+    success_url = reverse_lazy("blog:article_list")
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "blog/article_confirm_delete.html"
+    success_url = reverse_lazy("blog:article_list")
