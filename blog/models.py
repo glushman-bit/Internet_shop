776aaa90fdce313df735d5f3283ded40cb4031d9
Ivan Glushenkov <glushman966@gmail.com>
2026-05-02 18:20:08 +0300

    add id in models.py, update migration

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Article(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(
         max_length=100,
         verbose_name="Название статьи",
@@ -30,7 +31,7 @@ class Article(models.Model):
     )
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
 
     class Meta:
         verbose_name = "Статья"
