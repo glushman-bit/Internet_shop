ea7566987a8fa1635888dd6cd9fcaef5c8709299
Ivan Glushenkov <glushman966@gmail.com>
2026-05-05 23:11:15 +0300

    create forms.py for product

---
+++ catalog/forms.py
@@ -0,0 +1,39 @@
+from django.forms import ModelForm
+from django import forms
+from catalog.models import Product
+from django.core.exceptions import ValidationError
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control',
+            })
+
+class ProductForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Product
+        exclude = ('created_at', 'updated_at')
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+        }
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        name_lower = name.lower()
+        forbidden_words = [
+            'казино', 'биржа', 'криптовалюта', 'крипта', 'дешево',
+            'бесплатно', 'обман', 'полиция', 'радар',
+        ]
+        if name_lower in forbidden_words:
+            raise ValidationError('это слово запрещено')
+        return name
+
+    def clean_price(self):
+        price = self.cleaned_data['price']
+
+        if price <= 0:
+            raise ValidationError('Цена не может быть отрицательной или равной 0')
+        return price
