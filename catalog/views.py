dea255100fc88750d936de44c48aef318d859446
Ivan Glushenkov <glushman966@gmail.com>
2026-05-13 00:20:13 +0300

    update catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,15 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from itertools import product
+
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
 from django.views.generic import DetailView
 from django.views.generic import ListView
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, View
+from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponseForbidden
 
 from catalog.forms import ProductForm
 from catalog.models import Category
@@ -20,6 +24,8 @@ class ProductListView(ListView):
     paginate_by = 6
     ordering = ["-id"]
 
+    def get_queryset(self):
+        return Product.objects.order_by("-published")
 
 class ProductDetailView(DetailView):
     model = Product
@@ -34,11 +40,12 @@ class ProductDetailView(DetailView):
         return self.object
 
 
-class ProductCreateView(LoginRequiredMixin, CreateView):
+class ProductCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy("catalog:product_list")
+    permission_required = "catalog.can_add_product"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -46,11 +53,12 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
         return context
 
 
-class ProductUpdateView(LoginRequiredMixin, UpdateView):
+class ProductUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy("catalog:product_list")
+    permission_required = "catalog.can_change_product"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -58,10 +66,11 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         return context
 
 
-class ProductDeleteView(LoginRequiredMixin, DeleteView):
+class ProductDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Product
     template_name = "catalog/product_confirm_delete.html"
     success_url = reverse_lazy("catalog:product_list")
+    permission_required = "catalog.delete_product"
 
 
 class ContactListView(ListView):
@@ -77,3 +86,18 @@ class ContactListView(ListView):
         print(f"name: {name},\n" f"phone: {phone},\n" f"message: {message}")
 
         return HttpResponse(f"Привет {name}, ваши данные приняты.")
+
+
+class ProductUnpublishView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
+
+        if not request.user.has_perm("catalog.can_unpublish_product"):
+            return HttpResponseForbidden("У вас не доступа для снятия продукта с публикации.")
+        elif not request.user.has_perm("catalog.delete_product"):
+            return HttpResponseForbidden("У вас нет доступа для удаления продукта.")
+
+        product.published = False
+        product.save()
+
+        return redirect("catalog:product_list")
